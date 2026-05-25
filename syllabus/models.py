242823ee1ae748f6299c725d35f152128abced84
Артем Бичеров <jaineut@gmail.com>
2026-05-25 13:13:39 +0300

    Add course and lesson models

--- syllabus/models.py
+++ syllabus/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     title = models.CharField(max_length=30, verbose_name="Название")
-    image = models.ImageField(upload_to="images/courses/", verbose_name="Превью")
+    image = models.ImageField(upload_to="images/courses/", verbose_name="Превью", blank=True, null=True)
     description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
@@ -17,8 +17,8 @@ class Course(models.Model):
 class Lesson(models.Model):
     title = models.CharField(max_length=50, verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
-    image = models.ImageField(upload_to="images/lessons/", verbose_name="Превью")
-    link = models.URLField(verbose_name="Ссылка_на_видео")
+    image = models.ImageField(upload_to="images/lessons/", verbose_name="Превью", blank=True, null=True)
+    link = models.URLField(verbose_name="Ссылка_на_видео", blank=True, null=True)
     course = models.ForeignKey(to=Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс")
 
     def __str__(self):
