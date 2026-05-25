b5907fa588eef35df2ec0a0786e7cf3128493b13
Артем Бичеров <jaineut@gmail.com>
2026-05-25 07:36:55 +0300

    Add settings for users app and media dir in settings module

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "syllabus",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -122,6 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
-# AUTH_USER_MODEL = "users.CustomUser"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+AUTH_USER_MODEL = "users.CustomUser"
+
