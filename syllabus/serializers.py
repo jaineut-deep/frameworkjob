7ba871a35cbd52e7f6c24bf0afc789c1553300ca
Артем Бичеров <jaineut@gmail.com>
2026-05-27 19:22:17 +0300

    Add lessons field for course serializer

--- syllabus/serializers.py
+++ syllabus/serializers.py
@@ -2,19 +2,20 @@ from rest_framework import serializers
 from syllabus.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True)
 
     def get_lesson_count(self, instance):
         return instance.lessons.count()
 
     class Meta:
         model = Course
-        fields = "__all__"
-
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
         fields = "__all__"
