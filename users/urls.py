29fc376f39360044760740400a290ccc7dfee8df
Артем Бичеров <jaineut@gmail.com>
2026-05-29 14:23:24 +0300

    Add a routes fot apiview of users app

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView
+from users.views import (PaymentListAPIView, CustomUserCreateAPIView, CustomUserListAPIView, CustomUserRetrieveAPIView,
+                         CustomUserUpdateAPIView, CustomUserDestroyAPIView)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,6 +12,11 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("payment/", PaymentListAPIView.as_view(), name="payment_list"),
+    path("user/", CustomUserListAPIView.as_view(), name="user_list"),
+    path("user/create/", CustomUserCreateAPIView.as_view(), name="user_create"),
+    path("user/<int:pk>/", CustomUserRetrieveAPIView.as_view(), name="user_detail"),
+    path("user/update/<int:pk>/", CustomUserUpdateAPIView.as_view(), name="user_update"),
+    path("user/delete/<int:pk>/", CustomUserDestroyAPIView.as_view(), name="user_delete"),
 
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
