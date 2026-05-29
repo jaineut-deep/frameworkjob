0bc938a37be4c2ccce73a14495f873cd518aec3b
Артем Бичеров <jaineut@gmail.com>
2026-05-29 23:09:17 +0300

    Add get permissions for course viewset

--- syllabus/views.py
+++ syllabus/views.py
@@ -1,11 +1,30 @@
 from syllabus.serializers import CourseSerializer, LessonSerializer
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 from syllabus.models import Course, Lesson
+from users.permissions import IsModerator, IsOwnerOrReadOnly
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [IsAuthenticated]
+        elif self.action == "create":
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        elif self.action == "retrieve":
+            self.permission_classes = [IsAuthenticated]
+        elif self.action == "update":
+            self.permission_classes = [IsAuthenticated]
+        elif self.action == "partial_update":
+            self.permission_classes = [IsAuthenticated]
+        elif self.action == "destroy":
+            self.permission_classes = [IsAuthenticated, ~IsModerator, IsOwnerOrReadOnly]
+
+
+
 
 
 class LessonListAPIView(generics.ListAPIView):
