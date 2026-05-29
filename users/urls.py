295338a1ec60273f37b433a0b1cfc848aec28fd1
Артем Бичеров <jaineut@gmail.com>
2026-05-29 06:22:45 +0300

    Include routes simplejwt's views to urls of users app

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from users.apps import UsersConfig
 from users.views import PaymentListAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
 
 urlpatterns = [
     path("payment/", PaymentListAPIView.as_view(), name="payment_list"),
+
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
