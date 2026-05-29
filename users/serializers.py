3dad8f8b18028aae014355004b42a1c67c192d0b
Артем Бичеров <jaineut@gmail.com>
2026-05-29 06:50:27 +0300

    Add a new permissions module and customuser serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from users.models import Payment
+from users.models import Payment, CustomUser
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -7,3 +7,10 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = ["email", "username", "password"]
