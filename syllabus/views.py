615107f0af5864a922f125b31e9a45764d02b5dd
Артем Бичеров <jaineut@gmail.com>
2026-05-30 08:03:02 +0300

    Add a fix for get_permissions and add endpoint's methods for course viewset

--- syllabus/views.py
+++ syllabus/views.py
@@ -1,8 +1,10 @@
+from rest_framework.exceptions import PermissionDenied
 from syllabus.serializers import CourseSerializer, LessonSerializer
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from syllabus.models import Course, Lesson
-from users.permissions import IsModerator, IsOwnerOrReadOnly
+from users.permissions import IsNotModerator, IsOwner
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -11,20 +13,51 @@ class CourseViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action == "list":
-            self.permission_classes = [IsAuthenticated]
+            return [IsAuthenticated()]
         elif self.action == "create":
-            self.permission_classes = [IsAuthenticated, ~IsModerator]
+            return [IsAuthenticated(), IsNotModerator()]
         elif self.action == "retrieve":
-            self.permission_classes = [IsAuthenticated]
+            return [IsAuthenticated()]
         elif self.action == "update":
-            self.permission_classes = [IsAuthenticated]
+            return [IsAuthenticated()]
         elif self.action == "partial_update":
-            self.permission_classes = [IsAuthenticated]
+            return [IsAuthenticated()]
         elif self.action == "destroy":
-            self.permission_classes = [IsAuthenticated, ~IsModerator, IsOwnerOrReadOnly]
+            return [IsAuthenticated(), IsNotModerator(), IsOwner()]
+        return super().get_permissions()
 
+    def list(self, request, *args, **kwargs):
+        if request.user.groups.filter(name="Moderators").exists():
+            queryset = self.get_queryset()
+        elif not request.user.groups.filter(name="Moderators").exists():
+            queryset = self.get_queryset().filter(owner=self.request.user)
+        else:
+            raise PermissionDenied("Недостаточно прав для отображения объектов.")
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user.groups.filter(name="Moderators").exists():
+            serializer = self.get_serializer(instance)
+        elif not request.user.groups.filter(name="Moderators").exists() and instance.owner == request.user:
+            serializer = self.get_serializer(instance)
+        else:
+            raise PermissionDenied("Недостаточно прав для отображения объекта.")
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        request = self.request
+        if request.user.groups.filter(name="Moderators").exists():
+            serializer.save()
+        elif (not request.user.groups.filter(name="Moderators").exists() and
+            self.get_queryset().filter(owner=request.user)):
+            serializer.save(owner=request.user)
+        else:
+            raise PermissionDenied("Недостаточно прав для обновления объекта.")
 
 
 class LessonListAPIView(generics.ListAPIView):
