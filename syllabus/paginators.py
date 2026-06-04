4751290ca90490226d3e82e8cf66d1d464210809
Артем Бичеров <jaineut@gmail.com>
2026-06-04 16:09:59 +0300

    Add a method with details to courselesson paginator class

--- syllabus/paginators.py
+++ syllabus/paginators.py
@@ -1,7 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class CourseLessonPaginator(PageNumberPagination):
-    page_size = 5
+    page_size = 2
     page_size_query_param = "page_size"
     max_page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'page_size': self.get_page_size(self.request),
+            'results': data
+        })
