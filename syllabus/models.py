c13399148ae504c9a9c6b624ce08e340f86d6c45
Артем Бичеров <jaineut@gmail.com>
2026-06-09 21:10:57 +0300

    Add price field to course class

--- syllabus/models.py
+++ syllabus/models.py
@@ -7,6 +7,7 @@ class Course(models.Model):
     description = models.TextField(verbose_name="Описание")
     owner = models.ForeignKey(to="users.CustomUser", on_delete=models.CASCADE, related_name="courses",
                               verbose_name="Владелец")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена", blank=True, null=True)
 
     def __str__(self):
         return self.title
