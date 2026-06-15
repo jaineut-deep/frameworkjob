3b8b8547bfb3ab9c5164ee84e4f7e7d7533590ca
Артем Бичеров <jaineut@gmail.com>
2026-06-15 22:15:54 +0300

    Set timezone for celery and django

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Moscow"
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -214,7 +214,7 @@ CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
 # Часовой пояс для работы Celery
-CELERY_TIMEZONE = "Europe/Moscow"
+CELERY_TIMEZONE = "UTC"
 
 # Флаг отслеживания выполнения задач
 CELERY_TASK_TRACK_STARTED = True
@@ -222,12 +222,12 @@ CELERY_TASK_TRACK_STARTED = True
 # Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
-# CELERY_BEAT_SCHEDULE = {
-#     'task-name': {
-#         'task': 'syllabus.tasks.',
-#         'schedule': timedelta(minutes=10),
-#     },
-# }
+CELERY_BEAT_SCHEDULE = {
+    'task-name': {
+        'task': 'users.tasks.block_user_after_month',
+        'schedule': timedelta(minutes=2),
+    },
+}
 
 CELERY_IMPORTS = (
     "syllabus.tasks",
