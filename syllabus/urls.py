97a581e79f608d38df94438bfd450887808c3450
Артем Бичеров <jaineut@gmail.com>
2026-07-01 16:35:48 +0300

    Add health check funk in syllabus urls for appropriate route

--- syllabus/urls.py
+++ syllabus/urls.py
@@ -3,11 +3,16 @@ from rest_framework.routers import DefaultRouter
 from syllabus.apps import SyllabusConfig
 from syllabus.views import (CourseViewSet, LessonListAPIView, LessonCreateAPIView, LessonRetrieveAPIView,
                             LessonUpdateAPIView, LessonDestroyAPIView, SubscriptionView)
+from django.http import HttpResponse
 
 app_name = SyllabusConfig.name
 
 router = DefaultRouter()
 router.register(r"courses", CourseViewSet, basename="courses")
+
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
@@ -16,4 +21,6 @@ urlpatterns = [
     path("lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson_update"),
     path("lesson/delete/<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson_delete"),
     path("subscription/", SubscriptionView.as_view(), name="subscription"),
+    # check
+    path('health/', health_check, name='health_check'),
 ] + router.urls
